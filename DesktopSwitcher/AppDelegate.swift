5debb93dc6cc9d02a55203ce4bcf0df58cb2735e
jeffesonQin <1247006353@qq.com>
2020-04-08 14:59:32 +0800

    Initial Commit

---
+++ DesktopSwitcher/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DesktopSwitcher
+//
+//  Created by Jefferson Qin on 2020/4/8.
+//  Copyright © 2020 Jefferson Qin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
