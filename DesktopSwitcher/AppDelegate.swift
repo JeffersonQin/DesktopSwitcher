ce899cecf5e327bb7e74ce440159d6be81d090be
jeffesonQin <1247006353@qq.com>
2020-04-09 00:44:14 +0800

    v1.0.0

--- DesktopSwitcher/AppDelegate.swift
+++ DesktopSwitcher/AppDelegate.swift
@@ -7,17 +7,73 @@
 //
 
 import Cocoa
+import UserNotifications
+import NotificationCenter
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, UNUserNotificationCenterDelegate {
 
-    @IBOutlet weak var window: NSWindow!
-
-
+    private var middleMouseDown: Bool = false
+    
+    @IBOutlet weak var statusMenu: NSMenu!
+    @IBAction func quitButtonClicked(_ sender: NSMenuItem) {
+        NSApplication.shared.terminate(self)
+    }
+    
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .badge, .sound, .carPlay]) { (granted, error) in
+            print("Premission Granted: \(granted)")
+        }
+        UNUserNotificationCenter.current().delegate = self
+        statusItem.button?.title = "Desktop Switcher"
+        statusItem.menu = statusMenu
+        statusItem.button?.image = NSImage.init(named: "icon")
+        NSWorkspace.shared.notificationCenter.addObserver(
+            self,
+            selector: #selector(spaceChanged),
+            name: NSWorkspace.activeSpaceDidChangeNotification,
+            object: nil
+        )
+        NSEvent.addGlobalMonitorForEvents(matching: NSEvent.EventTypeMask.otherMouseUp) { (event) in
+            self.middleMouseDown = false
+        }
+        NSEvent.addGlobalMonitorForEvents(matching: NSEvent.EventTypeMask.otherMouseDown) { (event) in
+            self.middleMouseDown = true
+        }
+        NSEvent.addGlobalMonitorForEvents(matching: NSEvent.EventTypeMask.scrollWheel) { (event) in
+            if event.scrollingDeltaY > 0 && self.middleMouseDown {
+                let content = UNMutableNotificationContent.init()
+                content.title = NSString.localizedUserNotificationString(forKey: "Switch Sucess", arguments: nil)
+                content.subtitle = NSString.localizedUserNotificationString(forKey: "Switched Left", arguments: nil)
+                let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: 0.1, repeats: false)
+                let request = UNNotificationRequest.init(identifier: "backToHome", content: content, trigger: trigger)
+                UNUserNotificationCenter.current().add(request) { (error) in
+                    print("Error: \(String(describing: error))")
+                }
+                NSWorkspace.shared.launchApplication("DLeft")
+                self.middleMouseDown = false
+            } else if event.scrollingDeltaY < 0 && self.middleMouseDown {
+                let content = UNMutableNotificationContent.init()
+                content.title = NSString.localizedUserNotificationString(forKey: "Switch Sucess", arguments: nil)
+                content.subtitle = NSString.localizedUserNotificationString(forKey: "Switched Right", arguments: nil)
+                let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: 0.1, repeats: false)
+                let request = UNNotificationRequest.init(identifier: "backToHome", content: content, trigger: trigger)
+                UNUserNotificationCenter.current().add(request) { (error) in
+                    print("Error: \(String(describing: error))")
+                }
+                NSWorkspace.shared.launchApplication("DRight")
+                self.middleMouseDown = false
+            }
+        }
     }
-
+    
+    @objc func spaceChanged() {
+        print("changed")
+    }
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
---
+++ README.md
@@ -0,0 +1,28 @@
+# DesktopSwitcher
+
+## Introduction
+
+DesktopSwitcher is an OSX application written in swift, which is served as an tool that can switching the desktop by clicking while scrolling the scrolling wheel on the mouse. The tool can especially bring convenience to those who seldom uses trackpad and intensively relies on mouse and keyboard. If you have any suggestion or advise, please feel free to contact me.
+
+Email: 1247006353@qq.com
+
+Wechat: 1247006353
+
+## Usage
+
+- Scrolling Up & Clicking the scrolling wheel: Move a space left
+- Scrolling Down & Clicking the scrolling wheel: Move a space right
+
+![](preview.gif)
+
+## Installation
+
+Download the release: https://github.com/JeffersonQin/DesktopSwitcher/releases
+
+1. Unpack the three “.app” files
+2. Move them into the Application Folder
+3. System Preference -> Security & Privacy -> Privacy -> Accessibility -> Add the three files.
+
+## LICENSE
+
+MIT LICENSE.
